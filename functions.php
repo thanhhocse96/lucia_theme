6269065be7255fe4003b2ef4676933963e9bc8a6
Unknown <thanh.hoquangcse96@gmail.com>
2018-03-27 23:40:01 +0700

    Change Checkout

--- functions.php
+++ functions.php
@@ -67,3 +67,32 @@ if ( version_compare( get_bloginfo( 'version' ), '4.7.3', '>=' ) && ( is_admin()
  * Note: Do not add any custom code here. Please use a custom plugin so that your customizations aren't lost during updates.
  * https://github.com/woocommerce/theme-customisations
  */
+
+ /**
+ * Credit: Thach Pham
+ * Ẩn mã bưu chính
+ * Ẩn địa chỉ thứ hai
+ * Đổi tên Bang / Hạt thành Tỉnh / Thành
+ * Đổi tên Tỉnh / Thành phố thành Quận / Huyện
+ * 
+ * 
+ * @hook woocommerce_checkout_fields
+ * @param $fields
+ * @return mixed
+ */
+function tp_custom_checkout_fields( $fields ) {
+	// Ẩn mã bưu chính
+	unset( $fields['postcode'] );
+	
+	// Ẩn địa chỉ thứ hai
+	unset( $fields['address_2'] );
+	
+	// Đổi tên Bang / Hạt thành Tỉnh / Thành
+	$fields['state']['label'] = 'Tỉnh / Thành';
+	
+	// Đổi tên Tỉnh / Thành phố thành Quận / Huyện
+	$fields['city']['label'] = 'Quận / Huyện';
+	
+	return $fields;
+   }
+   add_filter( 'woocommerce_default_address_fields', 'tp_custom_checkout_fields' );
