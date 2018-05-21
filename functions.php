a6993caa172346e0ca54d53eac7e78358692606c
Unknown <thanh.hoquangcse96@gmail.com>
2018-05-21 12:51:06 +0700

    Quan huyen update

--- functions.php
+++ functions.php
@@ -40,3 +40,30 @@ function woo_custom_post_date_column_time( $post ) {
 	
 }
 
+/**
+ * Ẩn mã bưu chính
+ * Ẩn địa chỉ thứ hai
+ * Đổi tên Bang / Hạt thành Tỉnh / Thành
+ * Đổi tên Tỉnh / Thành phố thành Quận / Huyện
+ * 
+ * 
+ * @hook woocommerce_checkout_fields
+ * @param $fields
+ * @return mixed
+ */
+function tp_custom_checkout_fields( $fields ) {
+  // Ẩn mã bưu chính
+  unset( $fields['postcode'] );
+  
+  // Ẩn địa chỉ thứ hai
+  unset( $fields['address_2'] );
+  
+  // Đổi tên Bang / Hạt thành Tỉnh / Thành
+  $fields['state']['label'] = 'Tỉnh / Thành';
+  
+  // Đổi tên Tỉnh / Thành phố thành Quận / Huyện
+  $fields['city']['label'] = 'Quận / Huyện';
+  
+  return $fields;
+ }
+ add_filter( 'woocommerce_default_address_fields', 'tp_custom_checkout_fields' );
